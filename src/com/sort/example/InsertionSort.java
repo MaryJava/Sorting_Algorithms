b55037a97e8e367772cae8e5ada4464e20d49749
Meri_Ghazaryan <mary88.ghazaryan@gmail.com>
2019-03-22 12:53:08 +0400

    Insertion Sort

---
+++ src/com/sort/example/InsertionSort.java
@@ -0,0 +1,28 @@
+package com.sort.example;
+
+import static com.sort.example.Util.print;
+
+public class InsertionSort {
+
+    void sort(int[] arr) {
+        for (int i = 1; i < arr.length; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        InsertionSort obj = new InsertionSort();
+        int[] arr = {12, 11, 13, 5, 6};
+        obj.sort(arr);
+        print(arr);
+    }
+}
+
+// Time Complexity: O(n*2)
---
+++ src/com/sort/example/Util.java
@@ -0,0 +1,9 @@
+package com.sort.example;
+
+public class Util {
+
+    static void print(int[] arr) {
+        for (int a : arr)
+            System.out.println(a);
+    }
+}
